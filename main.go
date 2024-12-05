04adb35a80d10edc2e5ecbb5492319bc65999009
Christian <86637723+digitalghost-dev@users.noreply.github.com>
2024-12-05 10:13:00 -0800

    Update main.go

--- main.go
+++ main.go
@@ -7,26 +7,37 @@ import (
 	"runtime/debug"
 )
 
+var version = "(devel)"
+
 // printVersion prints the application version
 func printVersion() {
+	if version != "(devel)" {
+		// Use version injected by -ldflags
+		fmt.Printf("Version: %s\n", version)
+		return
+	}
+
+	// Fallback to build info when version is not set
 	buildInfo, ok := debug.ReadBuildInfo()
 	if !ok {
-		fmt.Println("Unable to determine version information.")
+		fmt.Println("Version: unknown (unable to read build info)")
 		return
 	}
 
 	if buildInfo.Main.Version != "" {
 		fmt.Printf("Version: %s\n", buildInfo.Main.Version)
 	} else {
-		fmt.Println("Version: unknown")
+		fmt.Println("Version: (devel)")
 	}
 }
 
 func main() {
-	versionFlag := flag.Bool("version", false, "print version information")
+	// -v, --version flag retrives the currently installed version
+	currentVersionFlag := mainFlagSet.Bool("version", false, "Prints the current version")
+	shortCurrentVersionFlag := mainFlagSet.Bool("v", false, "Prints the current version")
 	flag.Parse()
 
-	if *versionFlag {
+	if *currentVersionFlag || *shortCurrentVersionFlag {
 		printVersion()
 		os.Exit(0)
 	}
